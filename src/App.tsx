c1c5a5b79b473b9982f318c5d1a1126cedbbb954
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2025-01-03 00:09:50 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
+import Products from './pages/Products';
 import Contact from './pages/Contact';
 import Delivery from './pages/Delivery';
 import Booking from './pages/Booking';
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/delivery" element={<Delivery />} />
             <Route path="/booking" element={<Booking />} />
@@ -27,5 +29,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
